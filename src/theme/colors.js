84437c80481b3d3034ae444e6fdf4fdbed3865b2
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-08-15 20:50:08 +0530

    colors theme added

---
+++ src/theme/colors.js
@@ -0,0 +1,13 @@
+export const light = {
+  border: "rgba(0, 0, 0, 0.1)",
+  wrapperShadow: "rgba(0, 0, 0, 0.01)",
+  background: "#FFFFFF",
+  font: "#202124",
+};
+
+export const dark = {
+  border: "rgba(255, 255, 255, 0.4)",
+  shadow: "rgba(0, 0, 0, 0.4)",
+  background: "#202124",
+  font: "#FFFFFF",
+};
